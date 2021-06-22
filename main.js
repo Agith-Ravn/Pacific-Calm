3f7fcc6eb573e52e9829fb80492f5b0daa7dbfba
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-06-22 23:38:27 +0200

    .

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const chatDiv = document.getElementsByClassName("home__chat")
+// Drop down menu
+function dropDowMenu() {
+    document.getElementById("dropdown").classList.toggle("dropdown__content--show");
+}
 
-function openChatBox(value) {
-    if (value) {
-        
+window.onclick = function(e) {
+    if (!e.target.matches('.dropdown__button')) {
+        var myDropdown = document.getElementById("dropdown");
+        if (myDropdown.classList.contains('dropdown__content--show')) {
+        myDropdown.classList.remove('dropdown__content--show');
+        }
     }
+}
+
+
+//Chat box
+function chatBox() {
+    document.querySelector(".chat__hidden").classList.toggle("chat__show");
 }
